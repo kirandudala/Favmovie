4f1d9f9e8e0f61a9d50f8230289193f781149756
kiran dudala <123096552+kirandudala@users.noreply.github.com>
2024-06-03 11:25:45 +0530

    updated local host

--- FavMovie-frontend/src/services/movies.js
+++ FavMovie-frontend/src/services/movies.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 class MoviesDataService {
   login(data) {
-    return axios.post("https://favmovie-backend.onrender.com/api/auth/login", data);
+    return axios.post("http://localhost:3001/api/auth/login", data);
   }
 
   signup(data) {
-    return axios.post("https://favmovie-backend.onrender.com/api/auth/register", data);
+    return axios.post("http://localhost:30001/api/auth/register", data);
   }
 
   addFavorite(movie, xsToken) {
-    return axios.post("https://favmovie-backend.onrender.com/api/movies/favorite", { movie: movie }, {
+    return axios.post("http://localhost:30001/api/movies/favorite", { movie: movie }, {
       headers: {
         'x-auth-token': xsToken
       }
@@ -18,7 +18,7 @@ class MoviesDataService {
   }
 
   getFavorites(xsToken) {
-    return axios.get("https://favmovie-backend.onrender.com/api/movies/favorites", {
+    return axios.get("http://localhost:30001/api/movies/favorites", {
       headers: {
         'x-auth-token': xsToken
       }
